49f2ca716f610fd64aacd5ddea05a5b5f26a7ab5
Nick Cromwell <cromwellnick@gmail.com>
2019-08-17 11:38:55 -0400

    Remove unused classes

--- src/Ranger.Services.Notifications.Data/Repositories/ITenantRepository.cs
+++
@@ -1,8 +0,0 @@
-using System;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.EntityFrameworkCore;
-
-namespace Ranger.Services.Notifications.Data {
-    public interface INotificationsRepository { }
-}
