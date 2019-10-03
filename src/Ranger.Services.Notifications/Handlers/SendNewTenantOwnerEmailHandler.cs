538fac0d818967fcd24bd383ed5043d61d15c3ab
Nick Cromwell <cromwellnick@gmail.com>
2019-10-02 20:18:51 -0400

    HTTP TO HTTPS

--- src/Ranger.Services.Notifications/Handlers/SendNewTenantOwnerEmailHandler.cs
+++ src/Ranger.Services.Notifications/Handlers/SendNewTenantOwnerEmailHandler.cs
@@ -28,7 +28,7 @@ namespace Ranger.Services.Notifications.Handlers
                     firstname = message.FirstName,
                 },
                 domain = message.Domain,
-                confirm = $"http://rangerlabs.io/confirmdomain?domain={message.Domain}&registrationKey={message.RegistrationCode}"
+                confirm = $"https://rangerlabs.io/confirmdomain?domain={message.Domain}&registrationKey={message.RegistrationCode}"
             };
             try
             {
